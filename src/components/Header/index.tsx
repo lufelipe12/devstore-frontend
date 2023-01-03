38fde8d80e87b84f57adc47bb3d15f8007027882
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-01-03 10:10:24 -0300

    feat: added first components header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { BiLogInCircle, BiRocket } from "react-icons/bi"
+
+const Header = () => {
+  return (
+    <header>
+      <h1>DEVSTORE</h1>
+      <div>
+        <BiLogInCircle /> <BiRocket />
+      </div>
+    </header>
+  )
+}
+
+export default Header
