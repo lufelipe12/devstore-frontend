8d459f1af08481fc0c35e45000e4ad143e4a9aea
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2023-01-03 17:16:41 -0300

    feat: added footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Routes from "./routes"
 import GlobalStyle from "./styles/global"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <GlobalStyle />
       <Header />
       <Routes />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,7 @@
+import { StyledFooter } from "./styles"
+
+const Footer = () => {
+  return <StyledFooter>Devnology © Todos os direitos reservados</StyledFooter>
+}
+
+export default Footer
